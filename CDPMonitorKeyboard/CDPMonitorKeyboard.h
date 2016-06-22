d8e07033c9953efbd392dd63a1fa7ea8e5e8f3df
cdpenggod <cdpenggod@163.com>
2016-06-22 16:10:39 +0800

    improve tableView mode

--- CDPMonitorKeyboard/CDPMonitorKeyboard.h
+++ CDPMonitorKeyboard/CDPMonitorKeyboard.h
@@ -24,38 +24,50 @@ typedef NSInteger CDPMonitorKeyboardMode;//模式
 
 @protocol CDPMonitorKeyboardDelegate <NSObject>
 
-//系统键盘出现时
+/**
+ *  系统键盘出现时
+ */
 -(void)didWhenKeyboardWillShow:(NSNotification *)notification;
 
-//系统键盘消失时
+/**
+ *  系统键盘消失时
+ */
 -(void)didWhenKeyboardWillHide:(NSNotification *)notification;
 
 @end
 
 
 
-
 @interface CDPMonitorKeyboard : NSObject
-
-//当isShowKeyboard==YES时,tableView滑动可判断YES时不收起键盘
-//如果项目里写了tableView滑动取消键盘的代码，必须在相关函数里面取消键盘代码前加入此判断
-@property (nonatomic,assign,readonly) BOOL isShowKeyboard;
 
 
 @property (nonatomic,weak) id <CDPMonitorKeyboardDelegate> delegate;
 
+/**
+ *  当isShowKeyboard==YES时,tableView滑动可判断YES时不收起键盘
+ *  如果项目里写了tableView滑动取消键盘的代码，必须在相关函数里面取消键盘代码前加入此判断
+ */
+@property (nonatomic,assign,readonly) BOOL isShowKeyboard;
 
-//获取其单例
+/**
+ *  当输入view高于键盘高度时是否仍然自动改变高度(默认为NO,仅限tableViewMode)
+ */
+@property (nonatomic,assign) BOOL changeWhenHigher;
+
+/**
+ *  获取CDPMonitorKeyboard单例
+ */
 +(CDPMonitorKeyboard *)defaultMonitorKeyboard;
 
-//自动监听调用方法(仅需调用一次，若果需要改变参数再调用)
-//一般模式时superView传输入视图所在的主视图即可
-//tableView模式时superView传输入视图所在的tableView
-//scrollView模式时superView传输入视图所在的scrollView
-//valueOfTheHigher为输入视图需要高出键盘的高度
-//mode为当前需要的模式(目前共有三种)
-//navigationControllerTopHeight为主视图的navigationBar高度+状态栏高度，如没有使用navigationController传0
-
+/**
+ *  自动监听调用方法(仅需调用一次，若果需要改变参数再调用)
+ *  一般模式时superView传输入视图所在的主视图即可
+ *  tableView模式时superView传输入视图所在的tableView
+ *  scrollView模式时superView传输入视图所在的scrollView
+ *  valueOfTheHigher为输入视图需要高出键盘的高度
+ *  mode为当前需要的模式(目前共有三种)
+ *  navigationControllerTopHeight为主视图的navigationBar高度+状态栏高度，如没有使用navigationController传0
+ */
 -(void)sendValueWithSuperView:(UIView *)superView higherThanKeyboard:(NSInteger)valueOfHigher andMode:(CDPMonitorKeyboardMode)mode navigationControllerTopHeight:(NSInteger)topHeight;
 
 
