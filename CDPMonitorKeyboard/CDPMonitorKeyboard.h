49c20a76057131d6ff1f7044bc71889888f88fb3
cdpenggod <cdpenggod@163.com>
2016-12-02 19:12:49 +0800

    improve

--- CDPMonitorKeyboard/CDPMonitorKeyboard.h
+++ CDPMonitorKeyboard/CDPMonitorKeyboard.h
@@ -12,9 +12,9 @@
 
 
 enum{
-    CDPMonitorKeyboardDefaultMode=0,//一般模式(输入视图在主视图，即在self.view上)
-    CDPMonitorKeyboardTableViewMode,//tableView模式(输入视图在tableView的cell.contentView上且tableView父视图为主视图，即self.view)
-    CDPMonitorKeyboardScrollViewMode,//scrollView模式(输入视图在scrollView上且scrollView父视图为主视图，即self.view)
+    CDPMonitorKeyboardDefaultMode=0,//一般模式(输入视图在UIView上或其子view中,即其下级体系中)
+    CDPMonitorKeyboardTableViewMode,//tableView模式(输入视图在tableView上或其子view中,即其下级体系中)
+    CDPMonitorKeyboardScrollViewMode,//scrollView模式(输入视图在scrollView上或其子view中,即其下级体系中)
 };
 typedef NSInteger CDPMonitorKeyboardMode;//模式
 
@@ -50,7 +50,7 @@ typedef NSInteger CDPMonitorKeyboardMode;//模式
 @property (nonatomic,assign,readonly) BOOL isShowKeyboard;
 
 /**
- *  当输入view高于键盘高度时是否仍然自动改变高度(默认为NO,仅限tableViewMode)
+ *  当输入view高于键盘高度时是否仍然自动改变高度(默认为NO,仅限tableViewMode\scrollViewMode)
  */
 @property (nonatomic,assign) BOOL changeWhenHigher;
 
@@ -61,19 +61,19 @@ typedef NSInteger CDPMonitorKeyboardMode;//模式
 
 /**
  *  自动监听调用方法(仅需调用一次，若果需要改变参数再调用)
- *  一般模式时superView传输入视图所在的主视图即可
+ *  一般模式时superView传输入视图所在视图即可
  *  tableView模式时superView传输入视图所在的tableView
  *  scrollView模式时superView传输入视图所在的scrollView
  *  valueOfTheHigher为输入视图需要高出键盘的高度
  *  mode为当前需要的模式(目前共有三种)
- *  navigationControllerTopHeight为主视图的navigationBar高度+状态栏高度，如没有使用navigationController传0
  */
--(void)sendValueWithSuperView:(UIView *)superView higherThanKeyboard:(NSInteger)valueOfHigher andMode:(CDPMonitorKeyboardMode)mode navigationControllerTopHeight:(NSInteger)topHeight;
+-(void)sendValueWithSuperView:(UIView *)superView higherThanKeyboard:(NSInteger)valueOfHigher andMode:(CDPMonitorKeyboardMode)mode;
 
 /**
  *  清空并重置所有相关数据,防止因为单例造成内存问题(推荐在dealloc中调用)
  */
 -(void)clearAll;
+
 
 
 
